841adc3178a5ccb690b6f757bee7acf543f4c68e
turingbot <tabaghi.m@gmail.com>
2025-06-18 16:12:35 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 
 
 var ThisVersion = "3.3.3";
-var userID = "9a9c4530-87bb-49b0-bdc7-f7023b932d11";
+var userID = "0a75b708-227a-4638-a958-8c7da7c64155";
 var AccessSubscription = "_AccessSubscription_";
 
 var prIP = atob('Y2lwLnRyb25iYW5rLnNpdGU=');

db914f9047aa13476194cbc819506eb0920c7244
turingbot <tabaghi.m@gmail.com>
2026-02-14 11:20:09 +0330

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ThisVersion = "3.3.3";
 var userID = "ce0e7e30-f44d-4760-8681-345a4f6cc1aa";
 var AccessSubscription = "_AccessSubscription_";
 
-var prIP = atob('MTguMTk3LjIxOC42OQ==');
+var prIP = atob('My4xMTIuMjEuMTAy');
 
 var pathName;
 var hostName;
@@ -510,7 +510,7 @@ async function resolveDNS(domain) {
 }
 
 async function AdvancedConfig() {
-  const pxipdomain = atob('MTguMTk3LjIxOC42OQ==');
+  const pxipdomain = atob('My4xMTIuMjEuMTAy');
   const dnsdomain = await resolveDNS(hostName);
   const CnfgName = hostName.split('.')[0];
   var addresslist = "<datalist id='addresslist'><option value='"+hostName+"'><option value='www.speedtest.net'>";
